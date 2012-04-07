5836459ed29c0a4718f3c3952fb51b70014c4b05
Daniel Meißner <daniel@3st.mine.nu>
2012-04-07 21:52:11 +0200

    Added first, still empty, rspec test.

--- spec/dme/filestatus_config_spec.rb
+++ spec/dme/filestatus_config_spec.rb
@@ -2,11 +2,11 @@ require 'dme/filestatus/config'
 
 describe Dme::Filestatus::Config do
   before(:all) do
-    @example_config = Dme::Filestatus::Config.init(Pathname('examples/config.yml'))
+    @example_config = Dme::Filestatus::Config.init(Pathname('conf/config.yml.example'))
   end
 
   describe 'example config should be exist' do
-    Dme::Filestatus::Config.init(Pathname('examples/config.yml'))
+    Dme::Filestatus::Config.init(Pathname('conf/config.yml.example'))
 
     Dme::Filestatus::Config.config_exist?.should eql(true)
   end
